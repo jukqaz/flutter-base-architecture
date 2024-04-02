60a1a85b694d51eddd529de5058f9d6be243f5ae
Myeongho Ham (arthur) <arthur@liveanywhere.me>
2024-04-02 19:55:02 +0900

    chore: Log env

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_base_architecture/src/app.dart';
+import 'package:flutter_base_architecture/src/core/utils/logger.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
+  talker.log(Platform.environment);
+
   runApp(
     const ProviderScope(child: MainApp()),
   );
